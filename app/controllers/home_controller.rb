798063f29b9125bfe96f66db4a68471006ac211b
diogo <di-ogo_51@hotmail.com>
2019-08-27 19:02:14 -0300

    cru of people complete

---
+++ app/controllers/home_controller.rb
@@ -0,0 +1,26 @@
+class HomeController < ApplicationController
+    
+      def index
+        
+      end
+    
+      def show
+        
+      end
+    
+      def create
+        
+      end
+    
+      def update
+        
+      end
+    
+      def edit
+        
+      end
+    
+      def destroy
+         
+      end
+end
